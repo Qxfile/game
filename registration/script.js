2dc9bab199b7bfa5dc46cc5af8707e8e2ade33ae
Qxfile <narcoha3000@gmail.com>
2022-11-03 19:01:32 +0300

    доработка главной страницы

--- registration/script.js
+++ registration/script.js
@@ -3,10 +3,14 @@
 const registration = document.querySelector('.registration');
 const authorization = document.querySelector('.authorization');
 const btnRegAut = document.querySelector('.registration-button');
-
+const vkAnimation = document.querySelector('.authorization__vk');
+const regMiddle = document.querySelector('.registration-middle')
+const checkAunt = document.querySelector('.check-aunt');
+const widd = 550;
 console.log(authorization);
 console.log(registration)
 console.log(btnRegAut)
+console.log(vkAnimation)
 
 registration.addEventListener('click', (event) => {
     registration.classList.add('toggle-identification')
@@ -15,8 +19,9 @@ registration.addEventListener('click', (event) => {
         registration.style.order = '-1';
     }
     btnRegAut.querySelector('.button__change-text').innerHTML = '<b>Зарегестрироваться</b>';
-
-
+    vkAnimation.style.display = 'none';
+    regMiddle.style.display = 'flex';
+    checkAunt.style.display = 'none';
 });
 
 authorization.addEventListener('click', (event) => {
@@ -27,5 +32,8 @@ authorization.addEventListener('click', (event) => {
         registration.style.order = '0'
     }
     btnRegAut.querySelector('.button__change-text').innerHTML = '<b>Войти</b>';
-
+    btnRegAut.style = 'max-width:550px;';
+    vkAnimation.style.display = 'block';
+    regMiddle.style.display = 'none';
+    checkAunt.style.display = 'flex';
 });
--- script/script.js
+++ script/script.js
@@ -1,2 +1,44 @@
 'use strict'
 
+const startScreen = document.querySelector('.main__start-screen')
+console.log(startScreen)
+
+// let opacityMain = 0;
+// let stepOpacity = 0.1;
+
+setInterval(function () {
+    startScreen.classList.add('start-screen-first');
+    startScreen.classList.remove('start-screen-two');
+    startScreen.classList.remove('start-screen-three');
+}, 4000);
+setInterval(function () {
+    startScreen.classList.add('start-screen-two');
+    startScreen.classList.remove('start-screen-first');
+    startScreen.classList.remove('start-screen-three');
+}, 8000);
+setInterval(function () {
+    startScreen.classList.add('start-screen-three');
+    startScreen.classList.remove('start-screen-two');
+    startScreen.classList.remove('start-screen-first');
+}, 12000);
+
+
+// const intMargin = setInterval(function () {
+//     if (opacityMain === 330)
+//     {
+//         clearInterval(intMargin)
+//     }
+//     opacityMain++;
+//     console.log(opacityMain)
+//     startScreen.style.marginRight = `${opacityMain}px`
+// },1)
+
+// const opacity = setInterval(function () {
+// stepOpacity+=stepOpacity;
+// startScreen.style.opacity = `${stepOpacity}`;
+// console.log(startScreen.style.opacity = `${stepOpacity}`)
+// },1)
+// clearInterval(opacity);
+
+
+
